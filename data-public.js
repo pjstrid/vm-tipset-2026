e3a98583eecf85988c4284df98bb17fb8b3eabd7
jonathan strid <pjstrid@outlook.com>
2026-06-15 04:00:32 +0100

    added scorer

--- data-public.js
+++ data-public.js
@@ -322,7 +322,8 @@ const POOL_DATA = {
       {player:"Giovanni Reyna",       country:"USA",                     goals:1},
       {player:"Amad Diallo",          country:"Elfenbenskusten",         goals:1},
       {player:"Yasin Ayari",          country:"Sverige",                 goals:1},
-      {player:"Alexander Isak",       country:"Sverige",                 goals:1}
+      {player:"Alexander Isak",       country:"Sverige",                 goals:1},
+      {player:"Omar Rekik",           country:"Tunisien",                goals:1}
     ]
   }
 };
